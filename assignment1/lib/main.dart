adc6b1b1d2e2c750091df2b7096f19e0d26b602b
gitName <a@b.c>
2023-09-24 23:51:14 -0600

    Finish assignment 1

--- assignment1/lib/main.dart
+++ assignment1/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, todo
 
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 //https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e
 
@@ -25,11 +26,75 @@ class MyApp extends StatelessWidget {
           title: const Text("App1 - UI Layout"),
           backgroundColor: Colors.blue,
         ),
-        body: const Row(
+        body: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             //TODO: Put your code here to complete this app.
+            Column(
+              children: [
+                Container(
+                  height: 100, 
+                  width: 100, 
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    border: Border.all(color: Colors.black, width: 3)),
+                    child: const Center(
+                      child: Text("Container 1"),),),
+                Transform.rotate(angle: pi / 4, child: Container(
+                  height: 100, 
+                  width: 100, 
+                  decoration: const BoxDecoration(
+                    color: Colors.white,),
+                    child: const Center(
+                      child: Text("Container 2"),),),),
+              ],
+            ),
+            Column(
+              children: [
+                Expanded(child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 100, 
+                    width: 100, 
+                    alignment: Alignment.bottomCenter,
+                    decoration: const BoxDecoration(
+                      color: Colors.yellow,),
+                      child: const Text("Container 3"),),),),
+                Expanded(child: Padding(
+                  padding: const EdgeInsets.all(10.0), 
+                  child: Container(
+                    height: 100, 
+                    width: 100,
+                    alignment: Alignment.centerRight,
+                    decoration: const BoxDecoration(
+                      color: Colors.blue,),
+                      child: const Text("Container 4"),),),),
+              ],
+            ),
+            Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 100, 
+                    width: 100, 
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      border: Border.all(color: Colors.white, width: 3),
+                      shape: BoxShape.circle),
+                      child: const Center(
+                        child: Text("Container 5", style: TextStyle(color: Colors.white),),),),),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 100, 
+                    width: 100, 
+                    decoration: const BoxDecoration(
+                      color: Colors.red,),
+                      child: const Text("Con 6", style: TextStyle(fontSize: 30),),),),
+              ],
+            )
           ],
         ),
       ),
